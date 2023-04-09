c3bed6c7b950786b854cc633a510b0ca328b2c5f
Сергей Каращук <kspost01@gmail.com>
2023-04-09 11:03:32 +0300

    add update 09.04.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
+import { useEffect } from 'react';
+import Form from './Form';
+import { selectContacts, selectError, selectIsLoading } from 'redux/selectors';
+import { addContact, fetchContacts } from 'redux/operations';
+import { AppBox } from './App.styled';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ContactsList from './ContactsList';
+import { useDispatch, useSelector } from 'react-redux';
+
+export default function App() {
+  const dispatch = useDispatch();
+
+  const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  function notifiesAlert(numberContact, nameContact) {
+    return toast.error(
+      `${numberContact} is already in contacts under the name ${nameContact}.`
+    );
+  }
+
+  function checkСontact(newName) {
+    return contacts.some(contact => contact.name === newName);
+  }
+
+  function onSubmit(name, phone) {
+    if (checkСontact(phone)) {
+      return notifiesAlert(phone, name);
+    }
+
+    dispatch(addContact({ name, phone }));
+    toast.success(`Contact ${name} added successfully`);
+  }
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <AppBox>
+      <ToastContainer autoClose={2000} position="top-center" />
+      <h1>Phonebook</h1>
+      <Form onSubmit={onSubmit} />
+      <h2>Contacts</h2>
+      {isLoading && !error && <b>Request in progress...</b>}
+
+      {/* 
+      {!isLoading && contacts.length === 0 && <p>Contacts list is empty</p>} */}
+      {error && <p>Something went wrong, please try again later</p>}
+      <ContactsList />
+    </AppBox>
   );
-};
+}
---
+++ src/components/ContactsList/ContactsList.js
@@ -0,0 +1,31 @@
+import ContactItem from 'components/ContactListItem';
+import { Item, List, Wrapper } from './ContactsList.styled';
+import { useSelector } from 'react-redux';
+import { selectContactByName, selectIsLoading } from 'redux/selectors';
+import Filter from 'components/Filter';
+
+const ContactsList = () => {
+  const contacts = useSelector(selectContactByName);
+  const isLoading = useSelector(selectIsLoading);
+
+  return (
+    <Wrapper>
+      {contacts.length > 0 && (
+        <>
+          <Filter />
+          <List>
+            {contacts.map(contact => (
+              <Item key={contact.id}>
+                <ContactItem contact={contact} />
+              </Item>
+            ))}
+          </List>
+        </>
+      )}
+
+      {!isLoading && contacts.length === 0 && <p>Contacts list is empty</p>}
+    </Wrapper>
+  );
+};
+
+export default ContactsList;
